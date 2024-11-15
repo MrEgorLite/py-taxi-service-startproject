3fdc7367172d0cd6fa6d5c543d389997e62111d8
mregorlite <egor.infinity@gmail.com>
2024-11-15 16:23:43 +0200

    test fix

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = "django-insecure-s&l)1qx=lyeo2wgs-fuksj+r1$yvx)ra210msc*jj6uyo=t)&1" # noqua
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
